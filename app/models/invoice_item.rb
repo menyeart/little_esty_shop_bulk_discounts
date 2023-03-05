52bcf37f7984696af547d62db5c8902b3991d1cb
Matt Enyeart <menyeart1@gmail.com>
2023-03-05 14:06:14 -0700

    Add relations to make associations easier

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,6 +7,8 @@ class InvoiceItem < ApplicationRecord
 
   belongs_to :invoice
   belongs_to :item
+  has_one :merchant, through: :item
+  has_many :bulk_discounts, through: :item
 
   enum status: [:pending, :packaged, :shipped]
 
@@ -14,4 +16,14 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def applied_discount
+    bulk_discounts.
+    joins(:invoice_items)
+    .select("bulk_discounts.id as discount_id")
+    .where("bulk_discounts.quantity_threshold <= invoice_items.quantity and #{self.id} = invoice_items.id")
+    .order("bulk_discounts.percentage_discount desc")
+    .first
+  end
+
 end
